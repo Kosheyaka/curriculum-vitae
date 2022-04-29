ef8da21f71fa82312c860f364ac7970d3f727dca
Dmitry Volkov <d.volkov@bothelp.io>
2022-04-29 17:00:44 +0300

    info added

---
+++ src/interfaces/info.ts
@@ -0,0 +1,62 @@
+export interface IContact {
+  type: string;
+  text: string;
+  link: string;
+}
+
+export interface ILanguageCert {
+  certName: string;
+  certLink: string;
+}
+
+export interface ILanguage {
+  flag: string;
+  title: string;
+  level: string;
+  cert?: ILanguageCert;
+}
+
+export interface IEducation {
+  fullName: string;
+  faculty: string;
+  grade: string;
+}
+
+export type ITextStyle = 'regular'| 'bold' | 'mono' | 'inherit';
+
+type IText = {
+  text: string;
+  style: ITextStyle;
+};
+
+type ITextList = {
+  items: IText[];
+};
+
+type INewLine = { isNewLine: true };
+type IDivider = { isDivider: true };
+
+export type IAboutMeItem = IText | IDivider | INewLine | ITextList;
+export type ICareer = IText | IDivider | INewLine | ITextList;
+
+export type IStackCategory = 'Frontend' | 'Backend' | 'Databases' | 'Architecture';
+
+export type IStackItem = {
+  toolName: string;
+  rating: string;
+  commentary: string;
+};
+
+export type IStack = {
+  category: IStackCategory;
+  items: IStackItem[];
+};
+
+export type InfoType = {
+  contacts: IContact[];
+  languages: ILanguage[];
+  education: IEducation;
+  aboutMe: IAboutMeItem[];
+  career: ICareer[];
+  stack: IStack[];
+};
